245903a635ae894ca7d09ff3d909a595114d2a21
fabriciogomesdev <fabriciogomesdev@gmail.com>
2025-04-22 18:09:07 -0300

    atualizacoes de requisicoes API REST

--- src/tasks/tasks/tasks.tsx
+++ src/tasks/tasks/tasks.tsx
@@ -2,120 +2,209 @@ import { useEffect, useState } from "react"
 import { useForm, SubmitHandler } from "react-hook-form";
 import "./styles.css"
 import { Check, CirclePlus, Trash2 } from "lucide-react";
+import axios from "axios";
+import { v4 as uuidv4 } from 'uuid';
+
 export default function Tasks() {
+  interface Task {
+    id: string;
+    taskDescription: string;
+    checked: boolean;
+    
+  }
 
-    interface Task extends Object {
-        taskDescription: string;
-        checked: boolean;
+  const { register, handleSubmit, reset } = useForm();
+  const [list, setList] = useState<Task | null>(null);
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [pendingTasks, setPendingTasks] = useState<Task[]>([]);
+  const [pendingDeletes, setPendingDeletes] = useState<Task[]>([]);
+
+  async function loadAllTasks() {
+    try {
+      const response = await axios.get<Task[]>("http://localhost:3000/tasks");
+      
+      setTasks(response.data)
+    } catch (error) {
+      console.log(error)
+
     }
+  }
 
-    const { register, handleSubmit, reset } = useForm();
-    const [list, setList] = useState<Task | null>(null)
-    const [tasks, setTasks] = useState<Task[]>([])
+  const handlePost = async (task: Task) => {
+    try {
+      const response = await axios.post("http://localhost:3000/tasks", task);
+      
 
+      
+      if (pendingTasks.length > 0) {
+        await retryPendingTasks();
+      }
+    } catch (error) {
+      console.error("Erro ao enviar a tarefa:", error);
 
+      
+      setPendingTasks((prev) => {
+        const updatedPending = [...prev, task];
+        
+        return updatedPending;
+      });
+    }
+  };
 
+  const handleDeleteRequest = async ( task: Task) => {
+    const failedDeletes = []
+      try {
+        const response = await axios.delete(`http://localhost:3000/tasks/${task.id}`);
+        
 
-    useEffect(() => {
-
-        if (list) {
-            setTasks(prevTasks => [...prevTasks, list]);
+        if (pendingDeletes.length > 0) {
+          await retryDeletingTasks();
         }
 
+      } catch (error) {
 
-    }, [list])
+        console.error("erro", error);
+        failedDeletes.push(task);
 
-    const handleToggleCheck = (indexChecker: Number) => {
-        setTasks(prevTasks =>
-            prevTasks.map((task, index) =>
-                index === indexChecker
-                    ? { ...task, checked: !task.checked }
-                    : task
-            ))
+      }
+    
+    setPendingDeletes(failedDeletes);
+  }
+
+  const retryDeletingTasks = async () => {
+    
+    
+    for (const tasks of pendingDeletes) {
+      await axios.delete(`http://localhost:3000/tasks/${tasks.id}`);
+      
     }
+  }
 
-    const handleDelete = (indexChecker: Number) => {
-        setTasks(prevTasks => prevTasks.filter((_, index) => index !== indexChecker))
+  const retryPendingTasks = async () => {
+    
+
+    const failedTasks: Task[] = [];
+    for (const task of pendingTasks) {
+      try {
+        await axios.post("http://localhost:3000/tasks", task);
+        console.log("Tarefa pendente enviada com sucesso:", task);
+      } catch (error) {
+        console.error("Erro ao tentar reenviar a tarefa:", error);
+        failedTasks.push(task); 
+      }
     }
+    setPendingTasks(failedTasks);
+  };
 
-    return (
-        <div className="tasks">
-            <form
-                className="task-form"
-                onSubmit={handleSubmit((data) => {
-                    const newTask = {
-                        taskDescription: data.taskDescription,
-                        checked: false
-                    }
-                    setList(newTask);
-                    reset();
-                })}
-            >
-                <input {...register("taskDescription", { required: true })} placeholder="Adicione uma nova tarefa" className="new-task" />
-                <button type="submit" className="create">
-                    Criar
-                    <CirclePlus color="#F2F2F2" size={17}/>
-                    </button>
-            </form>
+  useEffect(() => {
+    if (list) {
+      setTasks((prevTasks) => [...prevTasks, list]);
+      handlePost(list);
+    }
+  }, [list]);
 
-            <div>
-                <div className="menu">
-                    <div className="tasks-count">Tarefas criadas <div className="tasks-count-number">{tasks.length}</div></div>
-                    <div className="concluded-tasks">
-                        Concluídas
-                        <div className="concluded-tasks-counter">
-                            {tasks.length === 0 
-                            ? "0" : `${tasks.filter(task => task.checked).length} de ${tasks.length}`}
-                            
-                        </div>
-                    </div>
-                </div>
-                <div className="tasks-display">
-               {tasks.length === 0 
-               ? (
-                <div className="no-tasks">
-                    <img src="src\assets\Clipboard.png" />
-                <p className="no-tasks-message">Você ainda não tem tarefas cadastradas.</p>
-                <p className="no-tasks-message-two">Crie tarefas e organize seus itens a fazer.</p>
-              </div>
-               ) : (
-                <div className="items">
-                    
-                <ul>
-                    {tasks.map((task, index) => (
-                        <li key={index}>
-                            <div className={`task-container `}
-                            >
-                                
-                                <div className="task-content">
-                                    <input
-                                        type="checkbox"
-                                        checked={task.checked}
-                                        onChange={() => handleToggleCheck(index)}
-                                        className="checker"
-                                    ></input>
+  useEffect(() => {
+    loadAllTasks();
+  }, [])
 
-                                    <span className={`task-description ${task.checked ? "checked" : "unchecked"}`}>{task.taskDescription}</span>
-                                </div>
+  const handleToggleCheck = (index: number) => {
+    setTasks((prevTasks) =>
+      prevTasks.map((task, idx) =>
+        idx === index ? { ...task, checked: !task.checked } : task
+      )
+    );
+  };
+
+  const handleDelete = (task: Task, index: number) => {
+    setTasks((prevTasks) => prevTasks.filter((_, idx) => idx !== index));
+    
+    handleDeleteRequest(task);
+  };
 
 
-                                <button
-                                    className="delete"
-                                    onClick={() => handleDelete(index)}
-                                >
-                                    <Trash2 size={17} />
 
-                                </button>
-                            </div>
-                        </li>
-                    ))}
-                </ul>
+  return (
+    <div className="tasks">
+      <form
+        className="task-form"
+        onSubmit={handleSubmit((data) => {
+          const newTask = {
+            taskDescription: data.taskDescription,
+            checked: false,
+            id: uuidv4()
+          };
+          setList(newTask);
+          reset();
+          
+        })}
+      >
+        <input
+          {...register("taskDescription", { required: true })}
+          placeholder="Adicione uma nova tarefa"
+          className="new-task"
+        />
+        <button type="submit" className="create">
+          Criar
+        </button>
+      </form>
+
+      <div>
+        <div className="menu">
+          <div className="tasks-count">
+            Tarefas criadas <div className="tasks-count-number">{tasks.length}</div>
+          </div>
+          <div className="concluded-tasks">
+            Concluídas
+            <div className="concluded-tasks-counter">
+              {tasks.length === 0
+                ? "0"
+                : `${tasks.filter((task) => task.checked).length} de ${tasks.length}`}
             </div>
-               )}
-               </div>
-
-            </div>
-
+          </div>
         </div>
-    )
+        <div className="tasks-display">
+          {tasks.length === 0 ? (
+            <div className="no-tasks">
+              <img src="src/assets/Clipboard.png" alt="Sem tarefas" />
+              <p className="no-tasks-message">
+                Você ainda não tem tarefas cadastradas.
+              </p>
+              <p className="no-tasks-message-two">
+                Crie tarefas e organize seus itens a fazer.
+              </p>
+            </div>
+          ) : (
+            <ul>
+              {tasks.map((task, index) => (
+                <li key={index}>
+                  <div className={`task-container`}>
+                    <div className="task-content">
+                      <input
+                        type="checkbox"
+                        checked={task.checked}
+                        onChange={() => handleToggleCheck(index)}
+                        className="checker"
+                      />
+                      <span
+                        className={`task-description ${task.checked ? "checked" : "unchecked"
+                          }`}
+                      >
+                        {task.taskDescription}
+                      </span>
+                    </div>
+                    <button
+                      className="delete"
+                      onClick={() => handleDelete(task, index)}
+                    >
+                      <Trash2 />
+                    </button>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
